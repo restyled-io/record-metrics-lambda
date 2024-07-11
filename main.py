200a089b3dce4dd39f674620a41151903124cce0
patrick brisbin <pbrisbin@gmail.com>
2024-07-11 12:01:48 -0400

    Mock AWS and Redis so we can unit test

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import json
 import logging
 import os
-import sys
 from urllib.parse import urlparse
 
 import boto3
-import redis
+from redis import Redis as RealRedis
 import requests
 import structlog
 
 
+class AWS:
+    def ssm_get_parameter(self, *args, **kwargs):
+        ssm = boto3.client("ssm")
+
+        return ssm.get_parameter(*args, **kwargs)
+
+    def cloudwatch_put_metric_data(self, *args, **kwargs):
+        cw = boto3.client("cloudwatch")
+
+        return cw.put_metric_data(*args, **kwargs)
+
+
+class Redis:
+    def setup(self, *args, **kwargs):
+        if self.redis is None:
+            self.redis = RealRedis(*args, **kwargs)
+
+        return self
+
+    def llen(self, queue):
+        return self.redis.llen(queue)
+
+
 def copy_event_to_message(_logger, _log_method, event_dict):
-    event_dict['message'] = event_dict['event']
+    event_dict["message"] = event_dict["event"]
 
     return event_dict
 
@@ -22,80 +44,95 @@ def get_logger(log_level_str):
         structlog.processors.StackInfoRenderer(),
         structlog.processors.format_exc_info,
         structlog.processors.TimeStamper(),
-        structlog.processors.CallsiteParameterAdder(), copy_event_to_message,
-        structlog.processors.JSONRenderer()
+        structlog.processors.CallsiteParameterAdder(),
+        copy_event_to_message,
+        structlog.processors.JSONRenderer(),
     ]
 
     log_level = {
-        'critical': logging.CRITICAL,
-        'error': logging.ERROR,
-        'warning': logging.WARNING,
-        'info': logging.INFO,
-        'debug': logging.DEBUG
+        "critical": logging.CRITICAL,
+        "error": logging.ERROR,
+        "warning": logging.WARNING,
+        "info": logging.INFO,
+        "debug": logging.DEBUG,
     }.get(log_level_str.lower())
 
     structlog.configure(
         processors=processors,
-        wrapper_class=structlog.make_filtering_bound_logger(log_level))
+        wrapper_class=structlog.make_filtering_bound_logger(log_level),
+    )
 
     return structlog.get_logger()
 
 
-def handler(_event, _context):
-    env = os.environ.get('ENV', 'prod')
-    queue = os.environ.get('QUEUE', 'restyled:agent:webhooks')
-    log_level = os.environ.get('LOG_LEVEL', 'info')
+def handler(event, context):
+    env = os.environ.get("ENV", "prod")
+    queue = os.environ.get("QUEUE", "restyled:agent:webhooks")
+    log_level = os.environ.get("LOG_LEVEL", "info")
     logger = get_logger(log_level)
-    dimensions = [{
-        'Name': 'Environment',
-        'Value': env
-    }, {
-        'Name': 'QueueName',
-        'Value': queue
-    }]
+
+    aws = AWS()
+    redis = Redis()
 
     try:
-        url = get_redis_url(env)
-
-        if url.scheme == "rediss":
-            r = redis.Redis(host=url.hostname, port=url.port,
-                            password=url.password, ssl=True, ssl_cert_reqs=None)
-        else:
-            r = redis.Redis(host=url.hostname, port=url.port,
-                            password=url.password)
-
-        depth = r.llen(queue)
-
-        logger.info('Putting depth metric',
-                    app='record-metrics',
-                    env=env,
-                    queue=queue,
-                    depth=depth)
-
-        cw = boto3.client('cloudwatch')
-        cw.put_metric_data(Namespace='Restyled',
-                           MetricData=[{
-                               'MetricName': 'QueueDepth',
-                               'Value': depth,
-                               'Unit': 'Count',
-                               'Dimensions': dimensions
-                           }])
-
-        return {'ok': True, 'env': env, 'queue': queue, 'depth': depth}
+        return handler_(aws, redis, env, queue, logger)
     except:
         logger.error("Exception", exc_info=True)
 
-        return {'ok': False, 'env': env, 'queue': queue}
+        return {"ok": False, "env": env, "queue": queue}
 
 
-def get_redis_url(env):
-    ssm = boto3.client('ssm')
+def handler_(aws, redis, env, queue, logger):
+    dimensions = [
+        {"Name": "Environment", "Value": env},
+        {"Name": "QueueName", "Value": queue},
+    ]
+
+    url = get_redis_url(aws, env)
+
+    if url.scheme == "rediss":
+        redis.setup(
+            host=url.hostname,
+            port=url.port,
+            password=url.password,
+            ssl=True,
+            ssl_cert_reqs=None,
+        )
+    else:
+        redis.setup(host=url.hostname, port=url.port, password=url.password)
+
+    depth = redis.llen(queue)
+
+    logger.info(
+        "Putting depth metric",
+        app="record-metrics",
+        env=env,
+        queue=queue,
+        depth=depth,
+    )
+
+    aws.cloudwatch_put_metric_data(
+        Namespace="Restyled",
+        MetricData=[
+            {
+                "MetricName": "QueueDepth",
+                "Value": depth,
+                "Unit": "Count",
+                "Dimensions": dimensions,
+            }
+        ],
+    )
+
+    return {"ok": True, "env": env, "queue": queue, "depth": depth}
+
+
+def get_redis_url(aws, env):
     token_parameter_name = "/restyled/%s/redis-url" % env
-    token_parameter = ssm.get_parameter(Name=token_parameter_name)
+    token_parameter = aws.ssm_get_parameter(Name=token_parameter_name)
 
-    return urlparse(token_parameter['Parameter']['Value'])
+    return urlparse(token_parameter["Parameter"]["Value"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     result = handler(None, None)
     print(json.dumps(result, indent=2))
